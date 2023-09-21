22d205c07ac29f4496f9f5124ab55463b112258b
Luigi Fernando <luigifcard3@gmail.com>
2023-09-21 19:46:41 -0300

    V2

--- EX5/src/entidades/salas/Consultorio.java
+++ EX5/src/entidades/salas/Consultorio.java
@@ -1,5 +1,10 @@
 package entidades.salas;
 
 public class Consultorio extends Sala {
+
+    public Consultorio(int numeroSala, int andar, String bloco) {
+        super(numeroSala, andar, bloco);
+        
+    }
     
 }

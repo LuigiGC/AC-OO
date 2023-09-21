fceb4565931427736f64f3c8c4280fbc4e60ed30
Luigi Fernando <luigifcard3@gmail.com>
2023-09-21 19:58:48 -0300

    P

--- EX5/src/entidades/salas/SalaCirurgia.java
+++ EX5/src/entidades/salas/SalaCirurgia.java
@@ -1,5 +1,10 @@
 package entidades.salas;
 
-public class SalaCirurgia {
+public class SalaCirurgia extends Sala{
+
+    public SalaCirurgia(int numeroSala, int andar, String bloco) {
+        super(numeroSala, andar, bloco);
+       
+    }
     
 }

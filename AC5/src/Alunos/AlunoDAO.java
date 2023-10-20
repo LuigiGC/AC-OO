c6c0468fae0ed325446d68028b562352f78d01b5
Luigi Fernando <142604987+LuigiGC@users.noreply.github.com>
2023-10-20 11:52:21 -0300

    Add files via upload

---
+++ AC5/src/Alunos/AlunoDAO.java
@@ -0,0 +1,50 @@
+package Alunos;
+import java.util.HashMap;
+
+public class AlunoDAO {
+
+   
+    HashMap <String, Aluno> alunos;
+    
+    public AlunoDAO(){
+        alunos = new HashMap<>();
+    }
+
+    public void adcionarAluno(String matricula, String nome, String curso){
+        Aluno aluno = new Aluno(matricula,nome,curso);
+        alunos.put(matricula,aluno);
+        
+    }
+    public void removerAluno(String matricula){
+        alunos.remove(matricula);
+    }
+
+    public void AtualizarCurso(String matricula,String novoCurso){
+        if (alunos.containsKey(matricula)){
+            Aluno aluno = alunos.get(matricula);
+            aluno.setCurso(novoCurso);
+
+        }else{
+            System.out.println("Aluno não cadastrado!");
+        }
+    }
+
+    public Aluno buscarAluno(String matricula){
+        return alunos.get(matricula);
+    }
+
+    public void listarAlunos() {
+        for (Aluno aluno : alunos.values()) {
+            System.out.println("Nome: " + aluno.getNome() + "\nMatrícula: " + aluno.getMatricula() + "\nCurso: " + aluno.getCurso() + "\n ---------------------------------------------");
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Aluno aluno : alunos.values()) {
+            sb.append(aluno.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+}
